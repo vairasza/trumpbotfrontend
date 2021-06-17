b3267d5ea9fe639a0b2be56ec9253259fee186a3
vairasza <vairasza@mailbox.org>
2021-06-18 00:37:20 +0200

    fixed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /* eslint-env node */
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 8001
 
 process.title = 'trumpbot_website'
 
-app.use(express.static(__dirname + '/app'))
+app.use(express.static(path.join(__dirname, 'app')))
 
 app.all('*', (req, res) => {
 	res.sendStatus(401)
