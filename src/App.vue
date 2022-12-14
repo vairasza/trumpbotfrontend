398d1eba8669b0f61ebd7df22c3a95930aa487c8
vairasza <vairasza@mailbox.org>
2022-12-14 22:31:33 +0100

    upgrade to vuejs

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <div>
+    <p>Text</p>
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <span>References:</span>
+    <br />
+    <span
+      >Avatars: Photo by
+      <a
+        href="https://unsplash.com/@libraryofcongress?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText"
+        rel="noreferrer nofollow"
+        target="_blank"
+        >Library of Congress</a
+      >
+      on
+      <a
+        href="https://unsplash.com/@libraryofcongress?/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText"
+        rel="noreferrer nofollow"
+        target="_blank"
+        >Unsplash</a
+      >
+      and Icon made by
+      <a
+        href="https://www.flaticon.com/authors/becris"
+        rel="noreferrer nofollow"
+        target="_blank"
+        >Becris</a
+      >
+      from
+      <a
+        href="https://www.flaticon.com"
+        rel="noreferrer nofollow"
+        target="_blank"
+        >www.flaticon.com</a
+      ></span
+    >
+    <br />
+    <span
+      >Background: Photo by
+      <a
+        href="https://unsplash.com/@mktgmantra?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText"
+        rel="noreferrer nofollow"
+        target="_blank"
+        >David Everett Strickler</a
+      >
+      on
+      <a
+        href="https://unsplash.com/@libraryofcongress?/s/photos/white-house?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText"
+        rel="noreferrer nofollow"
+        target="_blank"
+        >Unsplash</a
+      ></span
+    >
+  </footer>
+</template>
+
+<style scoped></style>
